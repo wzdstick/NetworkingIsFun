fbe6c837916f989b8bb683859125c8c7361e845c
chanju Jeon <agiletalk@wzd.com>
2013-03-20 16:53:14 +0900

    Adding the Table and Activity Indicator Views

--- NetworkingIsFun/ViewController.h
+++ NetworkingIsFun/ViewController.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate> {
+    UITableView *_tableView;
+    UIActivityIndicatorView *_activityIndicatorView;
+    NSArray *_movies;
+}
+
+@property (nonatomic, retain) UITableView *tableView;
+@property (nonatomic, retain) UIActivityIndicatorView *activityIndicatorView;
+@property (nonatomic, retain) NSArray *movies;
 
 @end
--- NetworkingIsFun/ViewController.m
+++ NetworkingIsFun/ViewController.m
@@ -14,10 +14,29 @@
 
 @implementation ViewController
 
+@synthesize tableView = _tableView, activityIndicatorView = _activityIndicatorView, movies = _movies;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Setting Up Table View
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.tableView.hidden = YES;
+    
+    // Setting Up Activity Indicator View
+    self.activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.activityIndicatorView.hidesWhenStopped = YES;
+    self.activityIndicatorView.center = self.view.center;
+    [self.view addSubview:self.activityIndicatorView];
+    [self.activityIndicatorView startAnimating];
+    
+    // Initializing Data Source
+    self.movies = [[NSArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
